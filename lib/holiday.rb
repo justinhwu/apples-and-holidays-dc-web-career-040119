49fda12278133f93ceb42bee6a4a5d6441a9e766
Justin Wu <justinhwu95@gmail.com>
2019-03-19 00:34:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   array = []
-  
+  holiday_hash
 end
 
 
