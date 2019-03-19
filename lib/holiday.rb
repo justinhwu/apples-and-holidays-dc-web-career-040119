26f2ee62ac4f10d356dcc7ccba0c5675c27e9ede
Justin Wu <justinhwu95@gmail.com>
2019-03-19 00:20:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,6 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 holiday_hash each do |key, value|
   puts "#{key.to_s.capitalize!}:"
+  value.each do |
 end
 
 def all_holidays_with_bbq(holiday_hash)
